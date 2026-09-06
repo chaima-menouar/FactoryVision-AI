6950bff345cb626aacbfc9c33dd24eda71204bc6
Chaima Menouar <chaimaamnouar1234@gmail.com>
2026-09-06 20:03:16 +0100

    Switch frontend to cinematic FactoryVision theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles.css'
-import './demo.css'
-import './future-light.css'
-import './industrial-theme.css'
+import './cinematic.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
