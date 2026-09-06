183c5672bc9c21d3f47a5842e004a3a397bacaaa
Chaima Menouar <chaimaamnouar1234@gmail.com>
2026-09-06 20:34:24 +0100

    Activate muted teal FactoryVision theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './cinematic.css'
 import './calm-industrial.css'
+import './teal-industrial.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
